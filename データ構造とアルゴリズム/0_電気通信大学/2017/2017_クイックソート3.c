1261fc55fb9cd24cbad7644be2c0c7683203b492
tat <bp20025@shibaura-it.ac.jp>
2024-07-29 20:44:24 +0900

    2024_0729_2044_Doutor_GrandEmio_Ekinaka

--- データ構造とアルゴリズム/0_電気通信大学/2017/2017_クイックソート3.c
+++ データ構造とアルゴリズム/0_電気通信大学/2017/2017_クイックソート3.c
@@ -4,7 +4,12 @@
 
 void swap( int *a, int *b ); // swap関数
 
-void Qsort( int a[], int lo, int hi ); // クイックソート関数
+void Qsort( int a[], int lo, int hi ); // クイックソート3関数
+
+void Qsort2( int a[], int lo, int hi ); // 参考：クイックソート2関数
+// 芝浦工業大学システム理工学部電子情報システム学科
+// 学籍番号を入力してください 電気通信大学
+
 
 void printArray( int a[], int lo, int hi ); // 配列の一覧表示
 
@@ -54,6 +59,25 @@ void Qsort( int a[], int lo, int hi ) {
     if( r+1 < hi ) Qsort( a, r+1, hi );
   }
 }
+
+void Qsort2( int a[], int lo, int hi ) { // クイックソート2関数
+  if (lo < hi) { // 要素が複数個存在
+    int pivot = a[lo]; // pivotは次のソートから除外されるので，無限ループに陥ることはない
+    int last = lo;
+    for ( int i = lo + 1; i <= hi; i++) { // pivot未満をLeftへ追いやる
+      if ( a[i] < pivot )
+	swap (&a[i], &a[++last]); // 1 ~ lastでpivot未満が担保
+    }
+    swap (&a[last], &a[lo]); // 0 ~ last - 1 でpivot未満が担保
+    
+    // last にpivot，last + 1 ~ hi でpivot 以上が担保
+    
+    if (lo < last - 1) Qsort2(a, lo, last - 1);
+    if ( last + 1 < hi ) Qsort2(a, last + 1, hi);
+  }
+}
+    
+    
 
 void printArray( int a[], int lo, int hi ) {
   for( int i = lo; i <= hi; i++ ) {
